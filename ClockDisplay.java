e5831f759f5d3121d7fed25b3c227a3f51241303
Jesus <jfr1013@gmail.com>
2015-11-19 12:40:30 +0100

    Cambiado a formato AM/PM

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -18,8 +18,8 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
-        horas = new NumberDisplay(23);
-        minutos = new NumberDisplay(59);
+        horas = new NumberDisplay(24);
+        minutos = new NumberDisplay(60);
         horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
     }
     
@@ -28,11 +28,25 @@ public class ClockDisplay
      */
     public ClockDisplay(int nuevaHora,int nuevoMinuto)
     {
-        horas = new NumberDisplay(23);
+        horas = new NumberDisplay(24);
         horas.setValue(nuevaHora);
-        minutos = new NumberDisplay(59);
+        minutos = new NumberDisplay(60);
         minutos.setValue(nuevoMinuto);
-        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        if (nuevaHora>=0 && nuevaHora<12){
+            horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " A.M.";
+        }
+        else if(nuevaHora==12){
+            horaActual = nuevaHora + ":" + minutos.getDisplayValue() + " P.M.";
+        }
+        else{
+            nuevaHora=nuevaHora-12;
+            if (nuevaHora>9){
+                horaActual = nuevaHora + ":" + minutos.getDisplayValue() + " P.M.";
+            }
+            else{
+                horaActual = "0" + nuevaHora + ":" + minutos.getDisplayValue() + " P.M.";
+            }
+        }
     }
     
     /**
@@ -43,7 +57,21 @@ public class ClockDisplay
     {
         horas.setValue(h);
         minutos.setValue(m);
-        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        if (h>=0 && h<12){
+            horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " A.M.";
+        }
+        else if(h==12){
+            horaActual = h + ":" + minutos.getDisplayValue() + " P.M.";
+        }
+        else{
+            h=h-12;
+            if (h>9){
+                horaActual = h + ":" + minutos.getDisplayValue() + " P.M.";
+            }
+            else{
+                horaActual = "0" + h + ":" + minutos.getDisplayValue() + " P.M.";
+            }
+        }
     }
     
     /**
@@ -51,7 +79,6 @@ public class ClockDisplay
      */
     public String getTime()
     {
-        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
         return horaActual;
     }
     
@@ -61,10 +88,44 @@ public class ClockDisplay
     
     public void timeTick()
     {
+        int horaAmPm = horas.getValue()-12;
+        int minAmPm = minutos.getValue() + 1;
         minutos.increment();
-        if (minutos.getValue() == 0){
-            horas.increment();
+        if (minAmPm == 60){
+            minAmPm = 0;
+            horaAmPm = horaAmPm + 1;
         }
-        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        if (horaAmPm == 12){
+            horaAmPm = 0;
+            horaActual = "0" + horaAmPm + ":" + "0" + minAmPm + " A.M.";
+        }
+        if (horas.getValue()<12){
+            if (horaAmPm<10 && minAmPm<10){
+                horaActual = "0" + horaAmPm + ":" + "0" + minAmPm + " A.M.";
+            }
+            else if (horaAmPm<10 && minAmPm>=10){
+                horaActual = "0" + horaAmPm + ":" + minAmPm + " A.M.";
+            }
+            else if (horaAmPm>=10 && minAmPm<10){
+                horaActual = horaAmPm + ":" + "0" + minAmPm + " A.M.";
+            }
+            else{
+                horaActual = horaAmPm + ":" + minAmPm + " A.M.";
+            }
+        }
+        else{
+            if (horaAmPm<10 && minAmPm<10){
+                horaActual = "0" + horaAmPm + ":" + "0" + minAmPm + " P.M.";
+            }
+            else if (horaAmPm<10 && minAmPm>=10){
+                horaActual = "0" + horaAmPm + ":" + minAmPm + " P.M.";
+            }
+            else if (horaAmPm>=10 && minAmPm<10){
+                horaActual = horaAmPm + ":" + "0" + minAmPm + " P.M.";
+            }
+            else{
+                horaActual = horaAmPm + ":" + minAmPm + " P.M.";
+            }
+        }
     }
 }
